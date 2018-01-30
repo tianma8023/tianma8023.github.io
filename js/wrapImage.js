62d13ee981b67d84455a04a75f91a9d1cd87536f
tianma8023 <1808931265@qq.com>
2018-01-31 07:21:04 +0800

    [Site][Update] Waifu tipshexo s --debughexo s --debug

--- js/wrapImage.js
+++ js/wrapImage.js
@@ -1,1 +1,34 @@
-function wrapImageWithFancyBox(){$("img").not(".sidebar-image img").not("#author-avatar img").not(".mdl-menu img").not(".something-else-logo img").each(function(){var a=$(this),t=a.attr("alt"),e=a.parent("a");e.size()<1&&(e=a.wrap('<a href="'+this.getAttribute("src")+'"></a>').parent("a")),e.attr("data-fancybox","images"),t&&e.attr("data-caption",t)}),$().fancybox({selector:'[data-fancybox="images"]',thumbs:!0,hash:!1,loop:!1})}$(document).ready(function(){wrapImageWithFancyBox()});
+$(document).ready(function() {
+    wrapImageWithFancyBox();
+});
+
+
+/**
+ * Wrap images with fancybox support.
+ */
+function wrapImageWithFancyBox() {
+    $('img').not('.sidebar-image img').not('#author-avatar img').not(".mdl-menu img").not(".something-else-logo img").each(function() {
+
+        var $image = $(this);
+        var imageCaption = $image.attr('alt');
+        var $imageWrapLink = $image.parent('a');
+
+        if ($imageWrapLink.size() < 1) {
+            $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
+        }
+
+        $imageWrapLink.attr('data-fancybox', 'images');
+        if (imageCaption) {
+            $imageWrapLink.attr('data-caption', imageCaption);
+        }
+
+    });
+
+    $().fancybox({
+        selector: '[data-fancybox="images"]',
+        thumbs: true,
+        hash: false,
+        loop: false,
+    });
+
+}
